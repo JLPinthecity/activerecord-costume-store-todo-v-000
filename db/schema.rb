f619b5645c7d781d4d141c98f55883f999ab4677
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:43:02 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -12,34 +12,34 @@
 
 ActiveRecord::Schema.define(version: 3) do
 
-  create_table "costume_stores", force: :cascade do |t|
-    t.string "name"
-    t.string "location"
-    t.integer "costume_inventory"
-    t.integer "num_of_employees"
-    t.boolean "still_in_business"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
-  end
-
   create_table "costumes", force: :cascade do |t|
     t.string "name"
     t.integer "price"
-    t.string "size"
+    t.text "size"
     t.text "image_url"
-    t.datetime "created_at"
-    t.datetime "updated_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "costumesstores", force: :cascade do |t|
+    t.string "name"
+    t.string "location"
+    t.integer "costume_inventory"
+    t.integer "number_of_employees"
+    t.boolean "in_business"
+    t.time "opening_time"
+    t.time "closing_time"
   end
 
   create_table "haunted_houses", force: :cascade do |t|
     t.string "name"
     t.string "location"
     t.string "theme"
-    t.float "price"
+    t.integer "price"
     t.boolean "family_friendly"
-    t.datetime "opening_date"
-    t.datetime "closing_date"
-    t.text "description"
+    t.date "opening_date"
+    t.date "closing_date"
+    t.text "long_description"
   end
 
 end
