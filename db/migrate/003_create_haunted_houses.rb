2c4e6359bdfd774e89ecba6f08e0ed0c76ae88c3
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:30:40 +0000

    Automatically backed up by Learn

--- db/migrate/003_create_haunted_houses.rb
+++ db/migrate/003_create_haunted_houses.rb
@@ -1,2 +1,15 @@
-class HauntedHouses < ActiveRecord::Base
+class CreateHauntedHouses < ActiveRecord::Migration[5.1]
+
+   def change
+     create_table :haunted_houses do |t|
+        t.string :name
+        t.string :location
+        t.string :theme
+        t.integer :price
+        t.boolean :family_friendly
+        t.date :opening_date
+        t.date :closing_date
+        t.text :long_description
+     end
+   end
 end
