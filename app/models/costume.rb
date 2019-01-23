367d152350e81189faccfa241ba2a3ff77a23a41
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:27:13 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -14,3 +14,21 @@ class CreateCostumes < ActiveRecord::Migration[5.1]
    end
 
 end
+
+
+
+
+class CreateCostumeStores < ActiveRecord::Migration[4.2]
+
+   def change
+     create_table :costumesstores do |t|
+       t.string :name
+       t.string :location
+       t.integer :costume_inventory
+       t.integer :number_of_employees
+       t.boolean :in_business
+       t.time :opening_time
+       t.time :closing_time
+     end
+   end
+end
