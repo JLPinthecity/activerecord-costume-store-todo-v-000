335db5b5942a1aefdf39dc4b6fbe7675ed2149c0
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:28:31 +0000

    Automatically backed up by Learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -1,2 +1,13 @@
-class Costumes < ActiveRecord::Base
+class CreateCostumes < ActiveRecord::Migration[5.1]
+
+   def change
+     create_table :costumes do |t|
+       t.string :name
+       t.integer :price
+       t.text :size
+       t.text :image_url
+       t.timestamps
+     end
+   end
+
 end
