904c6f394f8b1bc00bdf31fe68e496dde6eb46e9
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:18:38 +0000

    Automatically backed up by Learn

--- app/models/costume.rb
+++ app/models/costume.rb
@@ -1,7 +1,7 @@
 # Create your Costume class here
 # It should inherit from ActiveRecord::Base
 
-class CreateCostumes < ActiveRecord::Base
+class CreateCostumes < ActiveRecord::Migration[5.1]
 
    def change
      create_table :costumes do |t|
