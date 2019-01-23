dbb1d7cabf0911c6c24bb4c3bbdfcb6f8149447a
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:40:40 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -4,8 +4,8 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
        t.string :name
        t.string :location
        t.integer :costume_inventory
-       t.integer :number_of_employees
-       t.boolean :in_business
+       t.integer :num_of_employees
+       t.boolean :still_in_business
        t.datetime :opening_date
        t.datetime :closing_date
      end
