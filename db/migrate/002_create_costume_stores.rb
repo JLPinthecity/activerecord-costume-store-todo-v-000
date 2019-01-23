2a9e239e2904c14faec8d0db4ef19da570260313
Joann Pan <joannlpan@gmail.com>
2019-01-23 21:30:25 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,10 +3,12 @@ class CostumeStores < ActiveRecord::Migration[5.1]
    def change
      create_table :costume_stores do |t|
        t.string :name
-       t.integer :price
-       t.text :size
-       t.text :image_url
-       t.timestamps
+       t.integer :location
+       t.text :costume_inventory
+       t.text :number_of_employees
+       t.timestamps :in_business
+       t.time :opening_date
+       t.time :closing_date
      end
    end
 
